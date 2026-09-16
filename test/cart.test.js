585da4e52a5adf06d6aaf7d1fd2213de62b85b30
Backstop Fleet <fleet@example.com>
2026-09-16 22:46:35 +0000

    Revert "Total a cart at one discount rate"
    This reverts commit ef2443cb6e789ad942d54c2445a5e5ffd6ad5b76.

--- test/cart.test.js
+++
@@ -1,7 +0,0 @@
-const test = require('node:test');
-const assert = require('node:assert');
-const cart = require('../src/cart');
-
-test('cart totals every line at the same discount', () => {
-  assert.equal(cart([2, 1], 10), 270);
-});
